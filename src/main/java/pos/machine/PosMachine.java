a872b50984e7929fb38eef5769bef3c0f542f8f6
CHEUNJO2 <cheunjo2@oocl.com>
2021-11-30 20:37:03 +0800

    [pos-system] [joanne] feat: countItem

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,23 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        Map<String, Integer> quantityList = countItem(barcodes);
         return null;
     }
+
+    private Map<String, Integer> countItem(List<String> barcodeList) {
+        Map<String, Integer> result = new HashMap<>();
+        for(String item : barcodeList) {
+            result.merge(item, 1, Integer::sum);
+        }
+        return result;
+    }
+
+
 }
